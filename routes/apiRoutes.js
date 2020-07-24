27b44eb297263fc37297501b8379524b34de8061
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-07-23 17:32:34 -0700

    Testing api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var notesData = require('../data/noteData');
+var notesData = require('../db/db.json');
 
 
 module.exports = function(app) {
