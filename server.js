afc08890d09a8f8c2160867e1df8527f70a4e532
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-07-23 17:18:44 -0700

    Fixed configuration for notes

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public/assets/css/"));
+app.use(express.static(__dirname + "/public/assets/js/"));
 
 // ================================================================================
 // ROUTER
