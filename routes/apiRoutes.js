8a5db62a36b5504d54a3299b97d105882ee650b0
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-23 18:30:33 -0700

    Added promises

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,11 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 var noteJSON = require('../db/db.json');
 var noteListItems = require('../public/assets/js/index')
