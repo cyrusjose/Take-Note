f12af2cdbd185dd09f3692975a30ea17048c3621
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-24 18:18:41 -0700

    Testing api route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
