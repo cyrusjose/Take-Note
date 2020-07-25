2b988ce530716f5f3a41680ba8ff7adb8f508c14
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-24 17:45:35 -0700

    Testing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,13 +5,14 @@
 // ===============================================================================
 
 const router = require("express").Router();
+const dbNotesJSON = require('../db/db.json');
 
 const fs = require("fs");
 const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const noteJSON = "../db/db.json";
+const noteJSON = "db.json";
 
 router.get("/notes", function (req, res) {
   readFileAsync(noteJSON, "utf8", function (err, data) {
@@ -20,19 +21,19 @@ router.get("/notes", function (req, res) {
   });
 });
 
-router.post("/notes", function (req, res) {
+router.post("/api/notes", function (req, res) {
   var newNotes = req.body;
-
   readFileAsync(noteJSON, "utf8", function (err, data) {
     if (err) throw err;
     var readDb = JSON.parse(data);
     newNotes.id = readDb.length + 1;
     readDb.push(newNotes);
+  }).then(() =>
     writeFileAsync(noteJSON, JSON.stringify(readDb), function (err) {
       if (err) throw err;
       res.json({ success: true });
-    });
-  });
+    }).then(() => res.json(dbNotesJSON))
+  );
 });
 
 // router.delete('/notes/:id', funciont(req,res){
