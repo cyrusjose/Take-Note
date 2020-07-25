89c7ec83fbfd4f86a29c8e5948042492c46463fe
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-24 17:52:39 -0700

    Test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -18,7 +18,7 @@ const getNotes = () => {
 // A function for saving a note to the db
 const saveNote = (note) => {
   return $.ajax({
-    url: "/notes",
+    url: "/api/notes",
     data: note,
     method: "POST",
   });
