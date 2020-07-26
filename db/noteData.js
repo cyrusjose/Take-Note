ddfa2f32ebf767b08beebc7d6ca542bba1c14764
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-25 17:51:03 -0700

    Made pretty

--- db/noteData.js
+++ db/noteData.js
@@ -1,52 +1,52 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class NotesData {
+  constructor() {
+    this.idNum = 0;
+  }
 
-    constructor(){
-        this.idNum = 0;
-    }
-    
-    readNotes(){
-        return readFileAsync('db/db.json', 'utf8');
-    }
+  readNotes() {
+    return readFileAsync("db/db.json", "utf8");
+  }
 
-    writeNotes(note){
-        return writeFileAsync('db/db.json', JSON.stringify(note))
-    }
+  writeNotes(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
-    getNotes(){
-        return this.readNotes().then(notes => {
-            console.log(notes);
-            let noteArr;
-            // Try this block of code
-            try {
-                noteArr = [].concat(JSON.parse(notes))
-            }
-            // if there's an error
-            catch (err){
-                console.log(err);
-            };
-            return noteArr;
-        });
-    }
+  getNotes() {
+    return this.readNotes().then((notes) => {
+      console.log(notes);
+      let noteArr;
+      // Try this block of code
+      try {
+        noteArr = [].concat(JSON.parse(notes));
+      } catch (err) {
+        // if there's an error
+        console.log(err);
+      }
+      return noteArr;
+    });
+  }
 
-    addNotes(){
-        const {title, text} = note;
-        const newNote = {title, text, id: ++this.idNum};
-        return this.getNotes()
+  addNotes() {
+    const { title, text } = note;
+    const newNote = { title, text, id: ++this.idNum };
+    return (
+      this.getNotes()
         // Get all the notes from previous and add new notes to the end of the array
-        .then(notes => [...notes, newNote])
+        .then((notes) => [...notes, newNote])
         // Write notes in file and update
-        .then(update => this.writeNotes(update))
+        .then((update) => this.writeNotes(update))
         .then(() => newNote)
-    }
+    );
+  }
 
-    // deleteNotes(){
+  // deleteNotes(){
 
-    // }
+  // }
 }
 
 module.exports = new NotesData();
