72c2e4650bdb23b3580d251f4b9ab42d0f134c5b
Cyrus Jose <cyrus.jose@outlook.com>
2020-07-25 17:45:00 -0700

    Making text consistent

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-const express = require("express");
+const express = require('express');
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -16,26 +16,26 @@ const PORT = process.env.PORT || 3030;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public/assets/css/"));
-app.use(express.static(__dirname + "/public/assets/js/"));
+app.use(express.static(__dirname + '/public/assets/css/'));
+app.use(express.static(__dirname + '/public/assets/js/'));
 
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// The below points our server to a series of 'route' files.
+// These routes give our server a 'map' of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-const apiRoute = require("./routes/apiRoutes");
-const htmlRoute = require("./routes/htmlRoutes");
+const apiRoute = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoutes');
 
-app.use("/", htmlRoute);
-app.use("/api", apiRoute);
+app.use('/', htmlRoute);
+app.use('/api', apiRoute);
 
 // =============================================================================
 // LISTENER
-// The below code effectively "starts" our server
+// The below code effectively 'starts' our server
 // =============================================================================
 
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
+  console.log('App listening on PORT: ' + PORT);
 });
